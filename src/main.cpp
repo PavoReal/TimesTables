fc8f10876fd353d6ad0aca578e68ccd344df7b09
PavoReal <Garrison.peacock@gmail.com>
2015-11-08 18:02:48 -0800

    Added a third graphic

--- src/main.cpp
+++ src/main.cpp
@@ -51,9 +51,6 @@ int main()
         SDL_Delay(1000 / 60);
     }
 
-    SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
-    SDL_RenderClear(renderer);
-
     SDL_Delay(5000);
 
     for (auto k = 1.0f; k < 100.0f; k += 0.005f)
@@ -68,6 +65,40 @@ int main()
             auto y1 = sin(M_PI_2 * i / 200.0f);
             auto x2 = cos(M_PI_2 * (i * k) / 200.0f);
             auto y2 = sin(M_PI_2 * (i * k) / 200.0f);
+
+            SDL_RenderDrawLine(renderer,
+                               RoundReal((x1 + 1.0f) * HEIGHT / 2 + (WIDTH - HEIGHT) / 2.0f),
+                               RoundReal((y1 + 1.0f) * HEIGHT / 2),
+                               RoundReal((x2 + 1.0f) * HEIGHT / 2),
+                               RoundReal((y2 + 1.0f) * HEIGHT / 2));
+        }
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+                return 0;
+        }
+
+        SDL_Delay(1000 / 60);
+    }
+
+    SDL_Delay(5000);
+
+    for (auto k = 1.0f; k < 100.0f; k += 0.005f)
+    {
+        SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
+
+        for (auto i = 0; i < 200; ++i)
+        {
+            auto x1 = cos(2 * M_E * i / 200.0f);
+            auto y1 = sin(2 * M_E * i / 200.0f);
+            auto x2 = cos(2 * M_E * (i * k) / 200.0f);
+            auto y2 = sin(2 * M_E * (i * k) / 200.0f);
 
             SDL_RenderDrawLine(renderer,
                                RoundReal((x1 + 1.0f) * HEIGHT / 2 + (WIDTH - HEIGHT) / 2.0f),
